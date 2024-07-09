11f863ebc7a096ace826e60b1b7da5760679eb9b
luhuailiang <luhuailiang.andy@bytedance.com>
2024-07-09 11:51:39 +0800

    fix: 修改依赖

--- src/background/messages/googleTranslate.ts
+++ src/background/messages/googleTranslate.ts
@@ -1,5 +1,5 @@
 import type { PlasmoMessaging } from '@plasmohq/messaging';
-import { GoogleTranslate } from '~contents/shared/utils/translateUtils/index.js';
+import { GoogleTranslate } from '../../contents/shared/utils/translateUtils/index.js';
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   const message = await GoogleTranslate(req.body.selectedText, req.body.option);
